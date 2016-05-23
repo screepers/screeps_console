091effdc549fc853f87309b3de3f5c22f8872b05
Robert Hafner <tedivm@tedivm.com>
2016-05-22 22:14:32 -0700

    settings, dependencies, scaffolding

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Screeps Console',
+    'author': 'Robert Hafner',
+    'url': 'https://github.com/tedivm/screeps_console',
+    'download_url': 'https://github.com/tedivm/screeps_console/releases',
+    'author_email': 'tedivm@tedivm.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['screeps_console'],
+    'scripts': [],
+    'name': 'screeps_console'
+}
+
+setup(**config)
